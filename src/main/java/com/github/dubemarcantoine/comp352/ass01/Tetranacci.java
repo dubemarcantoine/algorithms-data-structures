48d83856b86bb234f4123681397ba7bd5ff4308d
Marc-Antoine Dube <madube94@gmail.com>
2017-09-24 19:59:55 -0400

    feat(tail-recursive): Add tail recursive Tetranacci implementation

--- src/main/java/com/github/dubemarcantoine/comp352/ass01/Tetranacci.java
+++ src/main/java/com/github/dubemarcantoine/comp352/ass01/Tetranacci.java
@@ -3,10 +3,11 @@ package com.github.dubemarcantoine.comp352.ass01;
 public class Tetranacci {
 
     public static void main(String[] args) {
-        System.out.println(new Tetranacci().binary(11));
+        System.out.println(new Tetranacci().binary(10));
+        System.out.println(new Tetranacci().tailRecursive(100));
     }
 
-    public int binary(int n) {
+    public long binary(int n) {
         if (n <= 2) {
             return 0;
         } else if (n <= 4) {
@@ -14,5 +15,21 @@ public class Tetranacci {
         } else {
             return binary(n - 1) + binary(n - 2) + binary(n - 3) + binary(n - 4);
         }
+    }
+
+    public long tailRecursive(int n) {
+        if (n <= 2) {
+            return 0;
+        } else if (n <= 4) {
+            return 1;
+        }
+        return tailRecursive(0, 0, 0, 1, n);
+    }
+
+    private long tailRecursive(long a, long b, long c, long d, int count) {
+        if(count <= 0) {
+            return a;
+        }
+        return tailRecursive(b, c, d, a+b+c+d, count-1);
     }
 }
