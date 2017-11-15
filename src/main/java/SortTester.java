445768ab9ce7d93925bf3296d42ebc10009e62a9
madube <madube94@gmail.com>
2017-11-14 22:32:55 -0500

    Initial commit

--- src/main/java/SortTester.java
+++ src/main/java/SortTester.java
@@ -3,19 +3,19 @@ import java.util.Collections;
 
 public class SortTester {
 
-	public static void main(String[] args) {
-		Tree <Number> tree = new A3BSTree<>();
-		Number a [] = new Number[10000000];
+    public static void main(String[] args) {
+        Tree<Number> tree = new A3BSTree<>();
+        Number a[] = new Number[10000000];
 
-		for (int i = 0; i < a.length; i++) a[i] = i;
-		System.out.printf("Initial%n%s%n",Arrays.toString(a));
-		
-		Collections.shuffle(Arrays.asList(a));
-		System.out.printf("Shuffled%n%s%n",Arrays.toString(a));
+        for (int i = 0; i < a.length; i++) a[i] = i;
+        System.out.printf("Initial%n%s%n", Arrays.toString(a));
 
-		TreeSort.sort(tree, a);
-		//TreeSort.sort(a);
-		System.out.printf("Sorted%n%s%n",Arrays.toString(a));
-	}
+        Collections.shuffle(Arrays.asList(a));
+        System.out.printf("Shuffled%n%s%n", Arrays.toString(a));
+
+        TreeSort.sort(tree, a);
+        //TreeSort.sort(a);
+        System.out.printf("Sorted%n%s%n", Arrays.toString(a));
+    }
 
 }
