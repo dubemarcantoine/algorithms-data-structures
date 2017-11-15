8d77b013987251e4f21677310c7c9560b0c6d2c0
madube <madube94@gmail.com>
2017-11-14 23:30:54 -0500

    Start Binary Search Tree

--- src/main/java/A3BSTree.java
+++ src/main/java/A3BSTree.java
@@ -2,12 +2,16 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-public class A3BSTree<E> implements Tree<E> {
+public class A3BSTree<E extends Comparable<E>> implements Tree<E> {
 
     private E[] tree;
+    private int size;
+    private int height;
 
     public A3BSTree() {
         this.tree = (E[]) new Object[0];
+        this.size = 0;
+        this.height = 0;
     }
 
     @Override
@@ -41,4 +45,17 @@ public class A3BSTree<E> implements Tree<E> {
         return 0;
     }
 
+    private int leftIndex(int parentIndex) {
+        return parentIndex * 2 + 1;
+    }
+
+    private int rightIndex(int parentIndex) {
+        return parentIndex * 2 + 2;
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = (int)Math.pow(2, this.height);
+        E[] newTree = (E[]) new Object[newCapacity];
+
+    }
 }
